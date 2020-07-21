6e869e353cff27f12e01394b1fd56582cfb526d8
wangff <1024904798@qq.com>
2020-07-21 20:41:09 +0800

    feat: init

--- src/main/java/com/wdcloud/kono/task/ResiterLmsUserByKonoTask.java
+++ src/main/java/com/wdcloud/kono/task/ResiterLmsUserByKonoTask.java
@@ -44,9 +44,9 @@ public class ResiterLmsUserByKonoTask {
      * 注册lms用户
      * @throws Exception
      */
-    @Scheduled(cron = "0 0/1 * * * ?")
+//    @Scheduled(cron = "0 0/1 * * * ?")
 //    @Scheduled(cron = "0/5 * * * * ?")
-//    @Scheduled(cron = "0 0 2 * * ?")
+    @Scheduled(cron = "0 0 2 * * ?")
     public void task() throws Exception {
       log.info("【ResiterLmsUserByKonoTask】开始时间:{}",System.currentTimeMillis());
       work();
